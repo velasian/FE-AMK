a84170af6afe4a7c14cc5d6aa4b2f66bd4bf742e
Ryan Syahrullah <syahrullahryan@gmail.com>
2025-10-01 13:39:56 +0800

    feat: add responsive AMK Portal login page

---
+++ src/router/index.js
@@ -0,0 +1,18 @@
+import { createRouter, createWebHistory } from 'vue-router'
+import LoginView from '../views/LoginView.vue'
+
+const router = createRouter({
+  history: createWebHistory(),
+  routes: [
+    {
+      path: '/',
+      name: 'login',
+      component: LoginView,
+    },
+  ],
+  scrollBehavior() {
+    return { top: 0 }
+  },
+})
+
+export default router
